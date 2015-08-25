cb534f3c6a93f1a9409c36072d9b44dfa10d58ba
F. Leandro C <f.catalan.v@hotmail.com>
2015-08-25 02:37:21 -0300

    BD avanzada
    BD con datos. Falta implementar imagenes.

---
+++ automotora_modelos.sql
@@ -0,0 +1,11 @@
+
+
+
+
+INSERT INTO marca VALUES(48, "DAIHATSU", "Japon")
+INSERT INTO modelo VALUES(121,48, "MODEL X9..","motor ABC1", 1231,"sd","12km/l","imagen","fecha","lista")
+
+DELETE FROM marca WHERE marca.id = 48
+DELETE FROM modelo WHERE modelo.marca_id=48
+DELETE FROM imagen WHERE imagen.modelo_marca_id =48
+DELETE FROM auto WHERE auto.modelo_marca_id = 48
